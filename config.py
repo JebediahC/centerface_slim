7d82c7c5f5a1ba94e8a4f01ba341631b8d6ea2cb
JebediahC <572301036@qq.com>
2022-05-19 22:37:38 +0800

    pb4

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ class Config:
     # training
     epoch = 100
     lr = 5e-4
-    batch_size = 64
+    batch_size = 48
     pin_memory = True
     num_workers = 0
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
